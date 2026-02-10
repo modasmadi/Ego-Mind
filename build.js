a7dcef9ee913dcecbeafdbdd2b4ea6f3a7cb34f2
modasmadi <mahmoudsmadi30@gmail.com>
2026-02-10 13:23:07 +0300

    Delete build.js

--- build.js
+++
@@ -1,8 +0,0 @@
-export function initBuild(world,canvas){
-canvas.onclick=e=>{
-let x=Math.floor(e.offsetX/world.size);
-let y=Math.floor(e.offsetY/world.size);
-if(world.map[y]&&world.map[y][x]!=null){
-world.map[y][x]=1;
-world.save();
-}}}
